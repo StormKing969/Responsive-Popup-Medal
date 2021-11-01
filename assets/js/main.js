489a59bd70362e8ac6029f537bc106b80573e190
StormKing969 <wijesinghesaj@gmail.com>
2021-11-01 01:12:06 -0400

    Final Product

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,24 @@
 /*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) => {
+    const openBtn = document.getElementById(openButton);
+    const modalContainer = document.getElementById(modalContent);
 
+    if (openBtn && modalContainer) {
+        openBtn.addEventListener("click", () => {
+            modalContainer.classList.add("show-modal")
+        });
+    };
+};
+
+showModal("open-modal", "modal-container");
 
 /*=============== CLOSE MODAL ===============*/
+const closeBtn = document.querySelectorAll(".close-modal");
+
+function closeModal() {
+    const modalCOntainer = document.getElementById("modal-container");
+
+    modalCOntainer.classList.remove("show-modal");
+}
+
+closeBtn.forEach(c => c.addEventListener("click", closeModal));
