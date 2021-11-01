4bc26b018aad4e7fb1936279d6b23f2ca8139784
StormKing969 <wijesinghesaj@gmail.com>
2021-11-01 01:38:16 -0400

    Complete Version

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,37 @@ function closeModal() {
 }
 
 closeBtn.forEach(c => c.addEventListener("click", closeModal));
+
+/*=============== SLIDER ===============*/
+var clickCounter = 0;
+
+document.querySelector(".background_changer").addEventListener("change", function() {
+    clickCounter += 1;
+    // console.log(clickCounter);
+
+    // Effect 2
+    if (clickCounter == 1) {
+        document.getElementById("slider").classList.toggle("move_1");
+
+        document.getElementById("modal-content").classList.remove("effect_1");
+        document.getElementById("modal-content").classList.add("effect_2");
+    }
+
+    // Effect 3
+    if (clickCounter == 2) {
+        document.getElementById("slider").classList.toggle("move_2");
+        
+        document.getElementById("modal-content").classList.remove("effect_2");
+        document.getElementById("modal-content").classList.add("effect_3");
+    }
+    
+    // Effect 1
+    if (clickCounter == 3) {
+        document.getElementById("slider").classList.remove("move_2");
+        document.getElementById("slider").classList.remove("move_1");
+        clickCounter = 0;
+
+        document.getElementById("modal-content").classList.remove("effect_3");
+        document.getElementById("modal-content").classList.add("effect_1");
+    }  
+});
